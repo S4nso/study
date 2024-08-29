9a0344a3dd4c9aa047529ba2ba90d6831064759a
S4nso <snp.sanso@gmail.com>
2024-08-29 20:45:11 +0900

    10926

--- baekjoon/src/1517.py
+++ baekjoon/src/1517.py
@@ -1,11 +1,11 @@
 i = int(input())
 num = list(map(int, input().split()))
-#sortnum = sorted(num)
+sortnum = sorted(num)
 cnt = 0
 for k in range(0, i):
-    #if num != sortnum:
+    if num != sortnum:
         for j in range(0, i-k-1):
             if num[j] > num[j+1]: num[j], num[j+1], cnt = num[j+1], num[j], cnt + 1
-    #else:
-    #    break
+    else:
+        break
 print(cnt)
