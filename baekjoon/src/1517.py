954b7a16d0de144ea840baf628ee2804a3b32965
S4nso <snp.sanso@gmail.com>
2024-09-02 18:58:32 +0900

    1517 수정

--- baekjoon/src/1517.py
+++ baekjoon/src/1517.py
@@ -1,3 +1,4 @@
+'''
 i = int(input())
 num = list(map(int, input().split()))
 sortnum = sorted(num)
@@ -8,4 +9,31 @@ for k in range(0, i):
             if num[j] > num[j+1]: num[j], num[j+1], cnt = num[j+1], num[j], cnt + 1
     else:
         break
+print(cnt)
+'''
+def merge(left, right):
+    global cnt
+    i, j, temp = 0, 0, []
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            temp.append(right[j])
+            j += 1
+            cnt += len(left) - i
+        else:
+            temp.append(left[i])
+            i += 1
+    return temp + left[i:] + right[j:]
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
+
+
+n = int(input())
+cnt = 0
+arr = list(map(int, input().split()))
+merge_sort(arr)
 print(cnt)
